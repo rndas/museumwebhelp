08d2d75a5f6e41381415a6bb6938f8a9a52d20f4
rndas <70765287+rndas@users.noreply.github.com>
2020-12-18 10:21:44 -0500

    Add files via upload
    index.html and style.css are the pages I'm working on right now for the anchor and broken Kahlo image layout

---
+++ webdesignfinal/script.js
@@ -0,0 +1,25 @@
+import HorizontalScroll from '@oberon-amsterdam/horizontal';
+
+new HorizontalScroll();
+
+
+
+//Click the hamburger menu to see the animation
+//You can click the X to close or click anywhere outside the menu
+
+$(document).ready(function(){
+	$('.hamburger-shell').click(function(){
+		$('#menu').slideToggle(300);
+		$('.top').toggleClass('rotate');
+		$('.middle').toggleClass('rotate-back');
+		$('.menu-name').toggleClass('bump');
+		$('.bg-cover').toggleClass('reveal');
+	});
+	$('.bg-cover').click(function(){
+		$('#menu').slideToggle(300);
+		$('.top').toggleClass('rotate');
+		$('.middle').toggleClass('rotate-back');
+		$('.menu-name').toggleClass('bump');
+		$('.bg-cover').toggleClass('reveal');
+	})
+});
